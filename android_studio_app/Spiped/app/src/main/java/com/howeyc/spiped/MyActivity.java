02616c1b2934be1680b73ef639d0402c2b937ff1
Chris Howey <chris@howey.me>
2014-11-15 17:30:53 -0500

    Fix file select in web view.

--- android_studio_app/Spiped/app/src/main/java/com/howeyc/spiped/MyActivity.java
+++ android_studio_app/Spiped/app/src/main/java/com/howeyc/spiped/MyActivity.java
@@ -6,6 +6,10 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.webkit.WebChromeClient;
 
+import android.webkit.ValueCallback;
+import android.net.Uri;
+import android.content.Intent;
+
 import go.Go;
 import go.spipedmobile.Spipedmobile;
 
@@ -13,6 +17,20 @@ import go.spipedmobile.Spipedmobile;
 public class MyActivity extends Activity {
 
     WebView browser;
+
+    private ValueCallback<Uri> mUploadMessage;  
+    private final static int FILECHOOSER_RESULTCODE=1;
+
+    @Override  
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {  
+        if(requestCode==FILECHOOSER_RESULTCODE)
+        {  
+            if (null == mUploadMessage) return;  
+            Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();  
+            mUploadMessage.onReceiveValue(result);  
+            mUploadMessage = null;  
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +45,37 @@ public class MyActivity extends Activity {
         browser = (WebView)findViewById(R.id.browser);
         browser.getSettings().setJavaScriptEnabled(true);
         browser.setWebViewClient(new WebViewClient());
-        browser.setWebChromeClient(new WebChromeClient());
+        browser.setWebChromeClient(new WebChromeClient()
+        {
+        //The undocumented magic method override  
+        //Eclipse will swear at you if you try to put @Override here  
+        // For Android 3.0+
+        public void openFileChooser(ValueCallback<Uri> uploadMsg) {  
+            mUploadMessage = uploadMsg;  
+            Intent i = new Intent(Intent.ACTION_GET_CONTENT);  
+            i.addCategory(Intent.CATEGORY_OPENABLE);  
+            i.setType("*/*");  
+            MyActivity.this.startActivityForResult(Intent.createChooser(i,"File Chooser"), FILECHOOSER_RESULTCODE);  
+        }
+
+        // For Android 3.0+
+        public void openFileChooser(ValueCallback uploadMsg, String acceptType) {
+            mUploadMessage = uploadMsg;
+            Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+            i.addCategory(Intent.CATEGORY_OPENABLE);
+            i.setType("*/*");
+            MyActivity.this.startActivityForResult(Intent.createChooser(i, "File Browser"), FILECHOOSER_RESULTCODE);
+        }
+
+        //For Android 4.1
+        public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture){
+            mUploadMessage = uploadMsg;  
+            Intent i = new Intent(Intent.ACTION_GET_CONTENT);  
+            i.addCategory(Intent.CATEGORY_OPENABLE);  
+            i.setType("image/*");  
+            MyActivity.this.startActivityForResult( Intent.createChooser( i, "File Chooser" ), MyActivity.FILECHOOSER_RESULTCODE );
+        }
+        });
         browser.loadUrl("http://localhost:56056");
     }
 }
